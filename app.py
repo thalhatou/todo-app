c1358028695e85b0edc63eeb256e14651df2f98d
Thalhatou Yahya <thalhatouyahya5352@gmail.com>
2022-07-29 11:41:31 +0100

    postgres db  and added

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
 from flask import Flask, render_template
-
+# sql alchemy 
+from flask_sqlalchemy import SQLAlchemy;
 app = Flask(__name__)
+
+
+# config file for db 
+uri = 'postgresql://postgres:7y8a1h64@localhost:5432/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] =  uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# initializing a db instance
+db = SQLAlchemy(app)
+# model blueprint for todo app
+class Todo(db.Model):
+  # __tablename__ = 'todos'
+  id = db.Column(db.Integer,primary_key=True)
+  description = db.Column(db.String(),nullable=False)
+
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html',data=[
- {
-    'description': 'Todo 1'
-  }, {
-    'description': 'Todo 2'
-  }, {
-    'description': 'Todo 3'
-  }
-    ])
+    return render_template('index.html',data=Todo.query.all())
 
 
 
