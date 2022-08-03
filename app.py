879f9925d3ab8e90fcff90f15b4eb1d3da90e87c
Thalhatou Yahya <thalhatouyahya5352@gmail.com>
2022-08-03 07:15:27 +0100

    Errors handling added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Importing the Flask class from the flask module.
-from flask import Flask, render_template,request,redirect,url_for
+from flask import Flask, render_template,request,redirect,url_for,jsonify
 
 # Importing the SQLAlchemy class from the flask_sqlalchemy module.
 from flask_sqlalchemy import SQLAlchemy;
@@ -29,11 +29,17 @@ db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
+    error = False
+    body = {}
     description = request.form.get('description')
+    description = request.get_json()['description']
     todo = Todo(description=description)
     db.session.add(todo)
     db.session.commit()
-    return redirect(url_for('index'))
+    return jsonify({
+      'description' : todo.description
+    })
+
 
 
 @app.route('/')
