e0028f98ab1cb745ebd4790e11ffdcd7f9b6227d
Thalhatou Yahya <thalhatouyahya5352@gmail.com>
2022-08-06 19:23:54 +0100

    upate and delete implemented

--- app.py
+++ app.py
@@ -30,19 +30,45 @@ class Todo(db.Model):
   def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
 
+# Creating the table in the database.
 db.create_all()
 
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
+  error = False
+  body = {}
+  try:
    description = request.get_json()['description']
    todo = Todo(description=description)
-   print(todo.description)
    db.session.add(todo)
-   db.session.commit();
-   return jsonify({
-        'description': todo.description
-     })
+   db.session.commit()
+   body['id'] = todo.id
+   body['completed'] = todo.completed
+   body['description'] = todo.description
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if error:
+    abort (400)
+  else:
+    return jsonify(body)
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+      completed = request.get_json()['completed']
+      todo = Todo.query.get(todo_id)
+      todo.completed = completed
+      db.session.commit()
+    except:
+      db.session.rollback()
+    finally:
+      db.session.close()
+    return redirect(url_for('index'))
 
 
 @app.route('/')
